5917889c561b429facdd750d065580e9aff96eee
souvikpaul01 <souvik.paul01@gmail.com>
2020-09-04 10:46:44 +0000

    Fix : Dynamic working without node

--- dynamic.py
+++ dynamic.py
@@ -22,24 +22,39 @@ def change(c):
 
         return r
 
+def run(c):
+    if c:
+        update_dy(c)
+        next_node = check_host(c)
+        nodesToChange[c]= ''
+        return run(next_node)
+    else:
+        for i in nodesToChange.keys():
+            print(i)
+
+        with open('exp.yaml','w') as yamlfile:
+            yaml.dump(cntnt, yamlfile)
+
 def update_dy(v):
-    
-    node_to_change = v + "_09"      #str(change(v))
-    original_node = v + "_xx"
+    node_to_change = v.replace("xx","09")      #str(change(v))
+    original_node = v 
     #node_to_change_next = check_host(original_node)
    # print(node_to_change_next)
     cntnt["topology_template"]["node_templates"][node_to_change] = copy.deepcopy( cntnt["topology_template"]["node_templates"][original_node])
     #del cntnt["topology_template"]["node_templates"][original_node]
-    for i in cntnt["topology_template"]["node_templates"][node_to_change]['requirements']:
-        i['host'] = i['host'].replace("xx",'09')
+    if 'requirements' in cntnt["topology_template"]["node_templates"][node_to_change].keys():   
+        for i in cntnt["topology_template"]["node_templates"][node_to_change]['requirements']:
+     #       print(i.keys())
+            if 'host' in i.keys():
+                i['host'] = i['host'].replace("xx",'09')
 
-    with open('exp.yaml','w') as yamlfile:
-        yaml.dump(cntnt, yamlfile)
+#        with open('exp.yaml','w') as yamlfile:
+#            yaml.dump(cntnt, yamlfile)
     #original_node = original_node.replace("_xx",'')
     #print(original_node)
-    node_to_change_next = check_host(original_node)
-    return update_dy(node_to_change_next)
-
+#        node_to_change_next = check_host(original_node)
+#        return update_dy(node_to_change_next)
+    
 
 def check_host(k):
     node = k 
@@ -51,15 +66,19 @@ def check_host(k):
                 return host_i
 
 
+
 if __name__ == "__main__":
     input_file  =  open(sys.argv[1],'r')
-    req = sys.argv[2]
+    req = sys.argv[2] + '_xx'
     yaml = YAML(typ='safe')
     yaml.default_flow_style = False
     yaml.sort_base_mapping_type_on_output = False
     cntnt =  yaml.load(input_file)
+
+    nodesToChange = {}
+    #findNodesToChange()
     
-    update_dy(req)
+    run(req)
     #check_host(req)
     input_file.close()
 
