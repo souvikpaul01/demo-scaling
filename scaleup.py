6a710730cc1a5591291c49aea036cc383698c128
mainak89 <mainak@ut.ee>
2020-10-08 15:30:50 +0300

    Fix : dynamic.py scaleup.py

--- scaleup.py
+++ scaleup.py
@@ -1,21 +1,17 @@
 import sys
 import os
-#need to use the other python file was module library to this file
-file_to_delete = "/home/ubuntu/opera/xopera-opera/examples/nginx_openstack/webApp-loadbalancer-node_exporter-TOSCA/demo-scaling/.opera/instances/nginx-lb_0"
+
+path = os.getcwd()
+relativePathToFile = "/.opera/instances/nginx-lb_0"
+file_to_delete = path + relativePathToFile
+
 try:
     os.remove(file_to_delete)
-    os.system('python dynamic.py service_test_gen.yaml site')
-#    sleep(10)
-#    os.system('opera deploy service_test_gen.yaml')
-
-#    os.system('opera deploy service_test.yaml')
+    os.system('python dynamic.py service.yaml site')
 
     print ('File deleted')
 except FileNotFoundError:
     print ('File not found:', file_to_delete)
 
-
-#os.system('python upscale.py service.yaml service_test.yaml')
-
-os.system('opera deploy service_test_gen.yaml')
+os.system('opera deploy service.yaml')
 
